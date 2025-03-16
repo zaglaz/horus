55909e678689c7d1d7c21aa375f3e56ffa67da6e
zaglaz <96348494+zaglaz@users.noreply.github.com>
2025-03-16 01:26:01 -0400

    Started working on an autorole feature for newly-joined users

--- horus/src/main/java/zaglaz/Horus.java
+++ horus/src/main/java/zaglaz/Horus.java
@@ -2,12 +2,20 @@ package zaglaz;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.rest.util.Permission;
+import discord4j.rest.util.PermissionSet;
 import zaglaz.command.commands.Greet;
 import zaglaz.command.commands.RandomNumber;
+import zaglaz.util.Autorole;
 
 public class Horus {
   private static final String BOT_TOKEN = ""; //replace with bot token
   private static final long GUILD_ID = 1L; //replace with your guildID
+  private static final PermissionSet autorolePermissions = PermissionSet.of( //replace with any explicit perms you'd like the autorole to have
+    Permission.SEND_MESSAGES, 
+    Permission.ATTACH_FILES, 
+    Permission.CHANGE_NICKNAME
+  );
 
   public static DiscordClient client = DiscordClient.create(BOT_TOKEN);
 
@@ -27,6 +35,10 @@ public class Horus {
       rand.buildCommand();
       rand.makeGuildCommand(GUILD_ID);
       rand.listenForRandomNumber();
+
+      //Enables Autorole
+      Autorole auto = new Autorole(botGateway, "Newcomer", new int[] {0, 255, 0}, false, true, autorolePermissions);
+      auto.giveAutoRole();
 
       //Ensures bot doesn't disconnect
       botGateway.onDisconnect().block();
---
+++ horus/src/main/java/zaglaz/util/Autorole.java
@@ -0,0 +1,55 @@
+package zaglaz.util;
+
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.guild.MemberJoinEvent;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Role;
+import discord4j.core.spec.RoleCreateSpec;
+import discord4j.rest.util.Color;
+import discord4j.rest.util.PermissionSet;
+import reactor.core.publisher.Mono;
+
+public class Autorole {
+    public final GatewayDiscordClient client;
+    public String roleName;
+    public Color roleColor;
+    public boolean isHoisted;
+    public boolean isMentionable;
+    public PermissionSet rolePermissions;
+
+    public Autorole(GatewayDiscordClient client, String roleName, int[] rgb, boolean isHoisted, 
+    boolean isMentionable, PermissionSet rolePermissions) {
+        this.client = client;
+        this.roleName = roleName;
+        this.roleColor = Color.of(rgb[0], rgb[1], rgb[2]);
+        this.isHoisted = isHoisted;
+        this.isMentionable = isMentionable;
+        this.rolePermissions = rolePermissions;
+    }
+
+    public void updatePermissions(PermissionSet newRolePermissions) {
+        this.rolePermissions = (this.rolePermissions == null)
+        ? newRolePermissions
+        : this.rolePermissions.and(newRolePermissions);
+    }        
+
+    public Mono<Role> makeAutoRole(Guild guild) {
+        return guild.createRole(RoleCreateSpec.builder()
+            .name(roleName)
+            .color(roleColor)
+            .hoist(isHoisted)
+            .mentionable(isMentionable)
+            .permissions(rolePermissions)
+            .build());
+    }
+
+    /* to-do: with .flatMap() and .filter() retrieve guild roles, see if we already have a role of the same name; 
+     * if we don't call makeAutoRole() and add it, then assign it to the newly-joined user regardless/
+    */
+    public void giveAutoRole() {
+        client.on(MemberJoinEvent.class)
+            .flatMap(event -> event.getGuild())
+            .onErrorResume(e -> Mono.empty())
+            .subscribe();
+    }
+}
